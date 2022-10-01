c43537c8019d48c4314c5950188fb89c263dd6ac
paratrooper-94 <ermilov1994@yandex.ru>
2022-10-01 13:57:56 +0300

    Update PHP-1_les-3_task-2__TRUE.php

--- 1-3__Basic-PHP-course/1-1__Basic-PHP-course_lesson-3/PHP-1_les-3_task-2__TRUE.php
+++ 1-3__Basic-PHP-course/1-1__Basic-PHP-course_lesson-3/PHP-1_les-3_task-2__TRUE.php
@@ -1,64 +1,59 @@
 <?php
 
+// $wishes = ["счастья", "здоровья", "богатства", "воображения", "терпения"];
+// $epithets = ["бесконечного", "крепкого", "огромного", "космического"];
+// $random_congratulation = [];
+
+// $name = readline("Введите ваше имя: ");
+
+// $name = ucfirst($name); /* сделаем на всякий случай имя с заглавной буквы */
+// $number = 3;
+
+// // можно было сделать через shuffle()
+// // перемешивает элементы и берет рандомное значение?
+
+// for ($i = 0; $i < $number; $i++) {
+//     // 3 раза генерируем случайные пары слов
+//     // в задании не было чтобы пары не повторялись
+//     $randWishes = array_rand($wishes);
+//     $randEritets = array_rand($epithets);
+
+//     // созраняем пары в массиве в виде строки с пробелом, для последнего добавляем и
+//     $random_congratulation[] = $epithets[$randEritets] . ' ' . $wishes[$randWishes];
+//     // как вариант проверяем что собираем последний элемент и делаем логику чтобы последний был с И
+//     // $random_congratulation[] = ($i != $number - 1) ? $str : 'и ' . $str;
+
+//     unset($wishnes[$randWishes]);
+//     unset($epithets[$randEpithets]);
+// }
+
+// // как другой вариант удаляем последний элемент и добавляем обратно вместе с И
+// $last = ' и ' . array_pop($random_congratulation);
+// // $random_congratulation[] = 'и ' . $last;
+
+// // из массива формируем строку для поздравления 
+// $string_congratulation = implode(', ', $random_congratulation) . $last;
+
+// echo "$name, от всего сердца поздравляю тебя с Днем Рождения, желаю $sting_congratulation.";
+
+
+// die();
+
 $wishes = ["счастья", "здоровья", "богатства", "воображения", "терпения"];
 $epithets = ["бесконечного", "крепкого", "огромного", "космического"];
-$random_congratulation = [];
 
-$name = readline("Введите ваше имя: ");
+$name = readline("Как зовут именинника?");
+$wishLimit = 3;
+$wishChosen = [];
 
-$name = ucfirst($name); /* сделаем на всякий случай имя с заглавной буквы */
-$number = 3;
-
-// можно было сделать через shuffle()
-// перемешивает элементы и берет рандомное значение?
-
-for ($i = 0; $i < $number; $i++) {
-    // 3 раза генерируем случайные пары слов
-    // в задании не было чтобы пары не повторялись
-    $randWishes = array_rand($wishes);
-    $randEritets = array_rand($epithets);
-
-    // созраняем пары в массиве в виде строки с пробелом, для последнего добавляем и
-    $random_congratulation[] = $epithets[$randEritets] . ' ' . $wishes[$randWishes];
-    // как вариант проверяем что собираем последний элемент и делаем логику чтобы последний был с И
-    // $random_congratulation[] = ($i != $number - 1) ? $str : 'и ' . $str;
-
-    unset($wishnes[$randWishes]);
-    unset($epithets[$randEpithets]);
+for ($i = 1; $i <= $wishLimit; $i++) {
+    $wishChosen[] = $epithets[array_rand($epithets) . " " . $wishes[array_rand($wishes)]];
 }
 
-// как другой вариант удаляем последний элемент и добавляем обратно вместе с И
-$last = ' и ' . array_pop($random_congratulation);
-// $random_congratulation[] = 'и ' . $last;
+$lastWish = array_pop($wishChoosen);
 
-// из массива формируем строку для поздравления 
-$string_congratulation = implode(', ', $random_congratulation) . $last;
+$congratulationText = " Дорогой $name, от всего сердца поздравляю тебя с днем рождения, желаю " . implode(',', $wishChosen) . " и " . $lastWish;
 
-echo "$name, от всего сердца поздравляю тебя с Днем Рождения, желаю $sting_congratulation.";
-
-
-die();
-
-
-
-
-// for ($i = -1; $i <= count($arrayWishes); $i++) {
-
-//     // берем случайный ключ из наших массивов слов
-//     $randomKeyWishes = array_rand($arrayWishes, 1);
-//     $randomKeyEpithets = array_rand($arrayEpithets, 1);
-
-//     // помещаем в массив $finalWishes первое слово из array
-//     $finalWishes[] = $arrayWishes[$randomKeyWishes];
-//     $finalWishes[] = $arrayEpithets[$randomKeyEpithets];
-
-//     // удаляем из исходного массива слово, которое уже использовали
-//     unset($arrayWishes[$randomKeyWishes]);
-//     unset($arrayEpithets[$randomKeyEpithets]);
-// };
-
-// echo "«Дорогой $userName, от всего сердца поздравляю тебя с днем рождения, желаю {$finalWishes[0]} {$finalWishes[1]}, {$finalWishes[2]} {$finalWishes[3]} и {$finalWishes[4]} {$finalWishes[5]}!»";
-// // /* «Дорогой Павел, от всего сердца поздравляю тебя с днем рождения, желаю здоровья крепкого, счастья огромного и богатства бесконечного!» */
-
+echo $congratulationText;
 
 // =====================================================
